39c0e33e337b0f18a3b12c8a4fcef9be8c8e02c6
Simon Wigh <126884012+swighCode@users.noreply.github.com>
2023-04-19 12:47:41 +0200

    Update OCR code to allow for user input in case of failed text reading from OCR.

--- OpenCv.py
+++ OpenCv.py
@@ -1,35 +1,59 @@
+
 import cv2
 import pytesseract
+import numpy as np
+import os
+import cv2
+import pytesseract
+import numpy as np
+import os
+#OCR function
+def ocr(image_path):
+    pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\bowst\\kth\\inda\\Private\\Tesser\\Tesseract-OCR\\tesseract.exe'
+    # Load the image
+    img = cv2.imread(image_path)
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\bowst\\kth\\inda\\Private\\Tesser\\Tesseract-OCR\\tesseract.exe'
+    # Convert the image to grayscale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Load the image
-img = cv2.imread("C:\\Users\\bowst\\kth\\inda\\Private\\Tesser\\pic2.png")
+    # Threshold the image to make the text more distinct
+    thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-# Convert the image to grayscale
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Perform OCR to extract text from the image
+    text = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
 
-# Threshold the image to make the handwriting more distinct
-thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # Split the text into lines and remove empty lines
+    lines = [line.strip() for line in text.split('\n') if line.strip()]
 
-# Find contours of handwriting regions in the image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Prompt the user to confirm the detected equations
+    print('OCR detected the following equations:')
+    for line in lines:
+        print(line)
+    confirm = input('Is this correct? (Y/N) ').strip().lower() == 'y'
 
-# Loop through each contour and extract the handwriting region
-for contour in contours:
-    # Get the bounding box of the contour
-    x, y, w, h = cv2.boundingRect(contour)
+    # If the detected equations are not correct, prompt the user to input them manually
+    if not confirm:
+        lines = []
+        num_equations = int(input('How many equations do you want to input? '))
+        for i in range(num_equations):
+            line = input(f'Enter equation {i + 1}: ')
+            lines.append(line)
 
-    # Extract the handwriting region
-    handwriting_region = thresh[y:y+h, x:x+w]
+    # Return the list of equations
+    return lines
 
-    # Apply OCR to recognize the text in the handwriting region
-    text = pytesseract.image_to_string(handwriting_region)
 
-    text = text.replace("\n", " ")
+#Main function
+def main():
+    #Get the path of the image
+    path = os.path.dirname(os.path.abspath(__file__))
+    image = os.path.join(path, 'pic2.png')
 
-    # Print the recognized text
+    #Call the OCR function
+    text = ocr(image)
 
-    # Print the recognized text without whitespace characters
+    #Print the recognized text
+    print(text)
 
-print (text)
+if __name__ == '__main__':
+    main()
